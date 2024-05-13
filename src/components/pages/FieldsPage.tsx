2442a5b320269fcb4339536a1e6eab9d71ee64f3
DVC <devine.vries@iqlogistica.com>
2024-05-13 13:06:48 +0200

    farms and fields

--- src/components/pages/FieldsPage.tsx
+++ src/components/pages/FieldsPage.tsx
@@ -1,6 +1,5 @@
 import { Divider, Grid, Paper } from "@mui/material";
 import FieldMapComponent from "../molecules/FieldMapComponent";
-import KanbanBoard from "../organisms/kanbanBoard/kanbanOverview";
 import FieldForm from "../organisms/FieldDetails";
 
 const FieldManagement = () => {
@@ -10,15 +9,13 @@ const FieldManagement = () => {
         <h1 className="title">Field Management</h1>
         <Divider />
       </Grid>
-      <Grid item xs={10}>
+      <Grid item xs={9}>
         <FieldMapComponent height={"485px"}></FieldMapComponent>
       </Grid>
-      <Grid item xs={2}>
+      <Grid item xs={3}>
           <FieldForm></FieldForm>
       </Grid>
-      <Grid item xs={12}>
-        <KanbanBoard></KanbanBoard>
-      </Grid>
+     
     </Grid>
   );
 };
--- src/routing.tsx
+++ src/routing.tsx
@@ -5,6 +5,7 @@ import OrganizationSettings from "./components/pages/OrganisationSettings";
 import Notes from "./components/pages/notesPage";
 import Activities from "./components/pages/ActivityPage";
 import FieldManagement from "./components/pages/FieldsPage";
+import FarmManagement from "./components/pages/FarmPage";
 
 const Routing: React.FC = () => {
   return (
@@ -14,6 +15,7 @@ const Routing: React.FC = () => {
         <Route path="/settings" element={<OrganizationSettings />} />
         <Route path="/notes" element={<Notes />} />
         <Route path="/fields" element={<FieldManagement />} />
+        <Route path="/farm" element={<FarmManagement />} />
         <Route path="/activity" element={<Activities />} />
         {/* Add more routes here */}
       </Routes>
