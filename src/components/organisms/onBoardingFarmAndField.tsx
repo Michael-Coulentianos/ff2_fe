fdcde752cd5c6cbfff43114bd1e18f776e386646
DVC <devine.vries@iqlogistica.com>
2024-05-23 10:48:13 +0200

    ui update onboarding

--- src/components/organisms/onBoardingFarmAndField.tsx
+++ src/components/organisms/onBoardingFarmAndField.tsx
@@ -5,7 +5,11 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import FormSection from "../molecules/DynamicFormSection";
 import FieldMapComponent from "../molecules/FieldMapComponent";
-import { ForwardRefRenderFunction, forwardRef, useImperativeHandle } from "react";
+import {
+  ForwardRefRenderFunction,
+  forwardRef,
+  useImperativeHandle,
+} from "react";
 
 const validationSchema = yup.object({
   farm: yup.string().required("Required"),
@@ -15,36 +19,37 @@ type OnBoardingFarmAndFieldProps = {
   onSubmit: (formData: any) => Promise<void>;
 };
 
-const OnBoardingFarmAndField: ForwardRefRenderFunction<unknown, OnBoardingFarmAndFieldProps> = ({ onSubmit }, ref) => {
-  const { control, handleSubmit, formState: { errors } } = useForm({
-    resolver: yupResolver(validationSchema)
+const OnBoardingFarmAndField: ForwardRefRenderFunction<
+  unknown,
+  OnBoardingFarmAndFieldProps
+> = ({ onSubmit }, ref) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
   });
 
   useImperativeHandle(ref, () => ({
-    submitForm: handleSubmit(onSubmit)
+    submitForm: handleSubmit(onSubmit),
   }));
 
   return (
     <Container>
-      <Typography variant="h4" gutterBottom>
+      <Typography variant="h5" gutterBottom>
         Add Farm & Field
       </Typography>
       <form>
-        <Box sx={{ mt: 4 }}>
-          <FormSection
-            title="Farm Details"
-            fields={[
-              { id: "farmName", label: "Farm Name", type: "text" }
-            ]}
-            control={control}
-            errors={errors}
-            columns={2}
-          />
-        </Box>
+        <FormSection
+          title="Farm Details"
+          fields={[{ id: "farmName", label: "Farm Name", type: "text" }]}
+          control={control}
+          errors={errors}
+          columns={2}
+        />
       </form>
-      <Box sx={{ mt: 4 }}>
-        <FieldMapComponent height={"400"}/>
-      </Box>
+      <FieldMapComponent height={"380"} />
     </Container>
   );
 };
