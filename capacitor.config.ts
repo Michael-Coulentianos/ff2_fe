e4ddcf4ec95849b8786ddda760e7e59a8438abe8
Ty <tyron.debeer@iqlogistica.com>
2024-05-27 09:06:08 +0200

    Native setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'Farmers Friendnpx',
+  webDir: 'build'
+};
+
+export default config;
